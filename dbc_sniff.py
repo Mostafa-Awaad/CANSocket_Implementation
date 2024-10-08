045008069f5c7c3f1caf241f0eb78e946a3f3428
mostafakamal <mostafamohamedkamal2000@gmail.com>
2024-10-08 11:38:20 +0000

    Make changes

--- dbc_sniff.py
+++ dbc_sniff.py
@@ -1,1 +1,8 @@
+#!/bin/sh
+#!/source/myenv/bin/cantools
 import can
+import sys
+import subprocess
+def install(cantools):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", cantools])
+import cantools
--- dbc_to_asc.py
+++ dbc_to_asc.py
@@ -21,7 +21,7 @@ with open('vehicle_speed_log.asc', 'w') as log_file:
     for i, speed in enumerate(vehicle_speed_values):
         # Ensure the signal name is correct
         try:
-            encoded_message = message.encode({'S01PID0D_VehicleSpeed': speed})  # Corrected signal name
+            encoded_message = message.encode({'S01PID0D_VehicleSpeed': speed})
             timestamp = timestamps[i]
             log_file.write(f"{timestamp} 1 {message.frame_id:X} Tx d {len(encoded_message)} {' '.join(f'{b:02X}' for b in encoded_message)}\n")
         except cantools.database.errors.EncodeError as e:
